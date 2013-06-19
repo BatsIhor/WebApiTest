a0ddf2d16426f4d786785c7cbb9d824aefb5ef94
Ihor Bats <batsihor@gmail.com>
2013-06-19 18:48:31 +0300

    Added test controller and a few rest methods

---
+++ WebApiTest/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+
+namespace WebApiTest
+{
+    public class TestController : ApiController
+    {
+        public string Get()
+        {
+            return "Test WebApi method!";
+        }
+        
+        public string Put()
+        {
+            return "Test WebApi method1!";
+        }
+
+        public string Post([FromBody]string item) //http://localhost:64981/test with item in the body
+        {
+            return "Test WebApi method2!";
+        }
+
+        public string Delete([FromBody]int index) //http://localhost:64981/test?0
+        {
+            return "Test WebApi method3!";
+        }
+    }
+}
