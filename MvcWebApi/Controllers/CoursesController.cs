7bb9954870b82fedff7ab8a1a0f6620060664133
Ihor Bats <batsihor@gmail.com>
2013-07-16 18:46:39 +0300

    ASP Mvc 4 WebApi prj added
    Added standard MVC 4 WebApi project.

---
+++ MvcWebApi/Controllers/CoursesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcWebApi.Controllers
+{
+    public class CoursesController : ApiController
+    {
+        static List<course> courses = new List<course>();
+
+        public CoursesController()
+        {
+            courses.Add(new course() { id = 0, title = "first" });
+            courses.Add(new course() { id = 1, title = "second" });
+            courses.Add(new course() { id = 2, title = "third" });
+            courses.Add(new course() { id = 3, title = "forth" });
+        }
+
+        public IEnumerable<course> Get()
+        {
+            return courses;
+        }
+
+        public course Get(int id)
+        {
+            return courses.Where((x)=> x.id == id).FirstOrDefault();
+            //if nulll than return 404
+        }
+    }
+
+    public class course
+    {
+        public int id;
+        public string title;
+    }
+}
--- WebApiTest/TestController.cs
+++ WebApiTest/TestController.cs
@@ -8,41 +8,53 @@ namespace WebApiTest
     {
         List<string> items = new List<string>() { "item1", "item2" };
 
-        public string Get()
+        public string Get(int index)
         {
+            if (index > 0)
+            {
+                return items.Aggregate((str1, str2) => str1 + " " + str2);
+            }
+            else
+            {
+                return "Modifyed Collection";
+            }
+        }
+
+        public string Put(int index, [FromBody]string item)
+        {
+            //PUT http://localhost:64981/test?index=0
+            //Content-Type: application/json
+            //Host: localhost:64981
+            //Accept: application/json
+            //Content-Length: 8
+
+            //Body
+            //"updated item"
+
+            items[index] = item;
             return items.Aggregate((str1, str2) => str1 + " " + str2);
         }
 
-        public void Put(int index, [FromBody]string item)
+        public string Post([FromBody]string item) //http://localhost:64981/test with item in the body
         {
-            items[index] = item;
-        }
-
-        public void Post([FromBody]string item) //http://localhost:64981/test with item in the body
-        {
-            //POST http://localhost:64981/test HTTP/1.1
-            //User-Agent: Fiddler
-            //Host: localhost:64981
-            //Content-Length: 6
-            //Content-Type: application/x-www-form-urlencoded
-
-            //=asdas
-            
-            //------------------OR ------------------
-
             //POST http://localhost:64981/test HTTP/1.1
             //User-Agent: Fiddler
             //Host: localhost:64981
             //Content-Length: 6
             //Content-Type: application/json
 
-            //"item"
+            //"new item"
             items.Add(item);
+            return items.Aggregate((str1, str2) => str1 + " " + str2);
         }
 
-        public void Delete([FromBody]int index) //http://localhost:64981/test?0
+        public string Delete([FromBody]int index) //http://localhost:64981/test?0
         {
+            //DELETE http://localhost:64981/test?0 HTTP/1.1
+
             items.RemoveAt(index);
+
+            return items.Aggregate((str1, str2) => str1 + " " + str2);
         }
     }
 }
